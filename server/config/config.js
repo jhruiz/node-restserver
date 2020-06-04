d06c9122bc190d7dbcbddf7e00092ac6af12ae58
Jaiber <jaiber.ruiz@correounivalle.edu.co>
2020-06-03 23:34:24 -0500

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,17 @@
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// Bae de datos
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://root:LB7oPuoipp1DRct8@cluster0-fr7v9.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
