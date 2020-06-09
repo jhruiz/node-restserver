1d253b0b1370b7607897892568ba80813096cc3b
Jaiber <jaiber.ruiz@correounivalle.edu.co>
2020-06-09 00:47:05 -0500

    login normal seccion 10 con tokens

--- server/config/config.js
+++ server/config/config.js
@@ -2,12 +2,19 @@
 process.env.PORT = process.env.PORT || 3000;
 
 // Entorno
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// Vencimiento del token
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//SEED -> semilla del token
+process.env.SEED = process.env.SEED || 'este-es-el-see-desarrollo';
 
 // Bae de datos
-
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
--- server/server.js
+++ server/server.js
@@ -17,7 +17,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(require('./routes/usuario'));
+// configuracion global de rutas
+app.use(require('./routes/index.js'));
+
 
 mongoose.connect(process.env.URLDB, {
         useNewUrlParser: true,
